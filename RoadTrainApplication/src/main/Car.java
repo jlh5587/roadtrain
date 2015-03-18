b21dd5440607306179679d76587a3594f20be1e0
jwa0008 <jwa0008@auburn.edu>
2015-03-18 14:34:42 -0500

    Update Car.java

--- RoadTrainApplication/src/main/Car.java
+++ RoadTrainApplication/src/main/Car.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Car implements Runnable{
 
 	public int speed = 0;
@@ -7,11 +14,13 @@ public class Car implements Runnable{
 	public Car head = null, tail = null;
 	public Truck truck = null;
 	public int id = 0;
-	boolean join = false;
+	public boolean join = false;
+	public File config_File = null;
 	
-	public Car(int id, int port){
+	public Car(int id, int port, File config_File){
 		this.id = id;
 		this.port = port;
+		this.config_File = config_File;
 	}
 	
 	public Car(int id){
@@ -23,11 +32,58 @@ public class Car implements Runnable{
 		{
 				try {
 					Thread.sleep(1000);
-				} catch (InterruptedException e) {
+					location[0] += speed;
+					this.update_config();
+				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			location[0] += speed;
 		}
+	}
+	public void update_config() throws IOException
+	{
+		//Read in the file
+		ArrayList<String> lines = new ArrayList<String>();
+		Scanner file_scan = new Scanner(config_File);
+		int index = 10;
+		String links = "";
+		String edit_Line = "";
+		String[] buf = null;
+		//adds each line of the file to the list of lines
+		while(file_scan.hasNext() && index != 0)
+		{
+			index--;
+			lines.add(file_scan.nextLine());
+		}
+		//closes the original scanner
+		file_scan.close();
+		Iterator<String> itr = lines.iterator();
+		//loops through each line to determine the distance from itself to that line.
+		while(itr.hasNext())
+		{
+			buf = itr.next().split(" ");
+			if(Integer.parseInt(buf[0]) != this.id)
+			{	
+				if(Integer.parseInt(buf[3]) - this.location[0] < 80 && Integer.parseInt(buf[3]) - this.location[0] > -80)
+				{
+					links += Integer.parseInt(buf[0]) + " ";
+				}
+			}
+		}
+		if(! lines.isEmpty())
+		{
+			buf = lines.get(this.id).split(" ");
+			edit_Line = buf[0] + " " + buf[1] + " " + buf[2] + " " + Integer.toString(this.location[0]) + " " + Integer.toString(this.location[1]) + " " + buf[5] + " " +  links;
+			lines.set(this.id, edit_Line);
+			PrintWriter clear = new PrintWriter(config_File);
+			clear.print("");
+			clear.close();
+			PrintWriter write = new PrintWriter(config_File);
+			for(int i = 0; i<lines.size(); i++){
+				write.println(lines.get(i));
+			}
+			write.close();
+		}
+		
 	}
 }
